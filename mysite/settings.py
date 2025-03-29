5e19de559ebdc928cb74c36b847bf41965c32579
alexml <alexgbrewster@gmail.com>
2025-03-29 17:51:22 -0400

    fixing settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY='django-insecure-*7+f$&+ju-fi2r@83ce*n!s-v8r39n2vk2tc%n^*mp657_@i^r'
+SECRET_KEY=os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
